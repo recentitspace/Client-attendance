7e6135b4a66c1406fbe804640c70b2e88d54592d
Hasina-Mohamed <hasuunamohamed2022@gmail.com>
2025-07-06 10:57:35 +0300

    changed the error of the dashboard

--- src/App.js
+++ src/App.js
@@ -13,13 +13,16 @@ import AdminAnnouncementsPage from './components/Announcements/Announcement';
 import QRGenerator from './components/Qr_code/QrCode';
 import TimeTableApp from './components/Timetable/TimeTableManagement';
 import WifiConfig from './components/SSID/Ssid';
-import TimetableForm from './components/Timetable/TimeTableForm'; 
-import { ThemeProvider as MaterialTailwindProvider } from "@material-tailwind/react";
+import TimetableForm from './components/Timetable/TimeTableForm';
 import TimetableDetails from './components/Timetable/TimetableDisplay';
 import ConfigManager from './components/ConfigManager';
 import HolidayManager from './components/Holiday/holiday';
-import { ThemeProvider } from './components/Context/ThemeContext';
 import ProfilePage from './components/Profile/ProfilePage';
+
+import { ThemeProvider as MaterialTailwindProvider } from "@material-tailwind/react";
+import { ThemeProvider } from './components/Context/ThemeContext';
+
+import PrivateRoute from './components/Auth/PrivateRoute';
 
 function App() {
   return (
@@ -28,23 +31,148 @@ function App() {
         <MaterialTailwindProvider>
           <Router>
             <Routes>
+              {/* Public Routes */}
               <Route path="/login" element={<SignIn />} />
               <Route path="/signup" element={<SignUp />} />
               <Route path="/forgot-password" element={<ForgotPassword />} />
-              <Route path="/" element={localStorage.getItem("token")? <Navigate to="/dashboard" />: <Navigate to="/login" />}/>
-              <Route path="/dashboard" element={<Dashboard />} />
-              <Route path="/profile" element={<ProfilePage />} />
-              <Route path="/employees" element={<EmployeeManagement />} />
-              <Route path="/attendance" element={<AttendanceManagement />} />
-              <Route path="/leave-requests" element={<LeaveRequests />} />
-              <Route path="/timetable" element={<TimeTableApp />} />
-              <Route path="/reports" element={<ReportsPage />} />
-              <Route path="/qrcode" element={<QRGenerator />} />
-              <Route path="/config" element={<ConfigManager />} />
-              <Route path="/form" element={<TimetableForm />} />
-              <Route path="/announcements" element={<AdminAnnouncementsPage />} />
-              <Route path="/timetables/:id" element={<TimetableDetails />} />
-              <Route path="/holiday" element={<HolidayManager />} />
+
+              {/* Redirect root path */}
+              <Route
+                path="/"
+                element={
+                  localStorage.getItem("token")
+                    ? <Navigate to="/dashboard" />
+                    : <Navigate to="/login" />
+                }
+              />
+
+              {/* Protected Routes */}
+              <Route
+                path="/dashboard"
+                element={
+                  <PrivateRoute>
+                    <Dashboard />
+                  </PrivateRoute>
+                }
+              />
+
+              <Route
+                path="/profile"
+                element={
+                  <PrivateRoute>
+                    <ProfilePage />
+                  </PrivateRoute>
+                }
+              />
+
+              <Route
+                path="/employees"
+                element={
+                  <PrivateRoute>
+                    <EmployeeManagement />
+                  </PrivateRoute>
+                }
+              />
+
+              <Route
+                path="/attendance"
+                element={
+                  <PrivateRoute>
+                    <AttendanceManagement />
+                  </PrivateRoute>
+                }
+              />
+
+              <Route
+                path="/leave-requests"
+                element={
+                  <PrivateRoute>
+                    <LeaveRequests />
+                  </PrivateRoute>
+                }
+              />
+
+              <Route
+                path="/timetable"
+                element={
+                  <PrivateRoute>
+                    <TimeTableApp />
+                  </PrivateRoute>
+                }
+              />
+
+              <Route
+                path="/reports"
+                element={
+                  <PrivateRoute>
+                    <ReportsPage />
+                  </PrivateRoute>
+                }
+              />
+
+              <Route
+                path="/qrcode"
+                element={
+                  <PrivateRoute>
+                    <QRGenerator />
+                  </PrivateRoute>
+                }
+              />
+
+              <Route
+                path="/config"
+                element={
+                  <PrivateRoute>
+                    <ConfigManager />
+                  </PrivateRoute>
+                }
+              />
+
+              <Route
+                path="/form"
+                element={
+                  <PrivateRoute>
+                    <TimetableForm />
+                  </PrivateRoute>
+                }
+              />
+
+              <Route
+                path="/announcements"
+                element={
+                  <PrivateRoute>
+                    <AdminAnnouncementsPage />
+                  </PrivateRoute>
+                }
+              />
+
+              <Route
+                path="/timetables/:id"
+                element={
+                  <PrivateRoute>
+                    <TimetableDetails />
+                  </PrivateRoute>
+                }
+              />
+
+              <Route
+                path="/holiday"
+                element={
+                  <PrivateRoute>
+                    <HolidayManager />
+                  </PrivateRoute>
+                }
+              />
+
+              <Route
+                path="/ssid"
+                element={
+                  <PrivateRoute>
+                    <WifiConfig />
+                  </PrivateRoute>
+                }
+              />
+
             </Routes>
           </Router>
         </MaterialTailwindProvider>
